199541694c427eecf74348994bf34e70111cba82
Erfan Ebrahimi <persionhost@gmail.com>
2023-07-26 09:21:30 +0330

    add option to order status

--- app/OrderStatus.php
+++ app/OrderStatus.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class OrderStatus extends Model
 {
     protected $guarded = ['id'];
+    protected $casts=[
+        'can_edit_good_value'=>'boolean',
+        'show_in_shiped_package'=>'boolean',
+        'show_in_received_package'=>'boolean',
+    ];
 }
