b44981fe02617b411f07c297e64980617df327c2
Erfan Ebrahimi <persionhost@gmail.com>
2023-07-30 00:04:05 +0330

    add price inside button

--- resources/views/member/stripe.blade.php
+++ resources/views/member/stripe.blade.php
@@ -220,7 +220,8 @@
                                         <div class="form-group row mb-0">
                                             <div class="col-md-6 offset-md-4">
                                                 <button type="submit" class="btn btn-primary">
-                                                    {{ __('Pay Now') }}
+                                                    {{ __('Pay') }} <strong
+                                                            class="font-18">{{'$'.number_format($package['invoice']->shipping_cost)}}</strong> {{ __('Now') }}
                                                 </button>
                                             </div>
                                         </div>
